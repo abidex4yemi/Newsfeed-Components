eabb3d44b9491c230189fd9960fc0db1a862f89a
abidex4yemi <abidex4yemi@gmail.com>
2019-05-15 23:45:38 +0100

    update stretch goal challenge

--- Article/Article.js
+++ Article/Article.js
@@ -1,4 +1,7 @@
 // Because classes are not hoisted you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
+const readMoreLabel = 'Read More';
+const doneLabel = 'Done';
+
 class Article {
 	constructor(domElement) {
 		// assign this.domElement to the passed in domElement
@@ -7,15 +10,66 @@ class Article {
 		this.expandButton = this.domElement.querySelector('.expandButton');
 
 		// Using your expandButton reference, update the text on your expandButton to say "expand"
-		this.expandButton.textContent = 'Expand';
-
+		this.expandButton.textContent = readMoreLabel;
 		// Set a click handler on the expandButton reference, calling the expandArticle method.
 		this.expandButton.addEventListener('click', this.expandArticle.bind(this));
 	}
 
+	getToggleButton() {
+		if (!this.expandButton.textContent || this.expandButton.textContent === doneLabel) {
+			this.expandButton.textContent = readMoreLabel;
+		} else {
+			this.expandButton.textContent = doneLabel;
+		}
+	}
+
 	expandArticle() {
+		this.getToggleButton();
 		// Using our reference to the domElement, toggle a class to expand or hide the article.
 		this.domElement.classList.toggle('article-open');
+	}
+}
+
+class ArticleGenerator {
+	constructor(article, data) {
+		this.article = article;
+		this.data = data;
+		this.createHeading();
+		this.createDate();
+		this.createParagraphs();
+		this.createButton();
+		this.appendToDOM();
+	}
+
+	appendToDOM() {
+		const articles = document.querySelector('.articles');
+		articles.insertAdjacentElement('afterbegin', this.article);
+	}
+
+	createHeading() {
+		const heading = document.createElement('h2');
+		heading.textContent = this.data.title;
+		this.article.append(heading);
+	}
+
+	createDate() {
+		const date = document.createElement('p');
+		date.textContent = new Date();
+		date.setAttribute('class', 'date');
+		this.article.append(date);
+	}
+
+	createParagraphs() {
+		const para1 = document.createElement('p');
+		para1.textContent = this.data.body;
+		this.article.insertAdjacentElement('beforeend', para1);
+	}
+
+	createButton() {
+		const button = document.createElement('span');
+		button.setAttribute('class', 'expandButton');
+		button.textContent = readMoreLabel;
+		this.article.insertAdjacentElement('beforeend', button);
 	}
 }
 
@@ -26,8 +80,26 @@ class Article {
 - With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the Article class.
 
 */
-const articles = document.querySelectorAll('.article');
 
+const addBtn = document.querySelector('.btn');
+
+addBtn.addEventListener('click', function(evt) {
+	evt.preventDefault();
+	const title = document.querySelector('.title').value;
+	const body = document.querySelector('.body').value;
+
+	const data = {
+		title,
+		body
+	};
+
+	const article = document.createElement('div');
+	article.setAttribute('class', 'article');
+
+	new ArticleGenerator(article, data);
+});
+
+const articles = document.querySelectorAll('.article');
 articles.forEach(article => {
 	new Article(article);
 });
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -11,3 +11,9 @@ const menuButton = document.querySelector('.menu-button');
 
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener('click', toggleMenu);
+
+window.addEventListener('click', evt => {
+	if (evt.target !== menuButton) {
+		menu.classList.remove('menu--open');
+	}
+});
